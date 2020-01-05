d60882b335f4f7d01fc01061b0027aeefc6958dd
EC2 Default User <ec2-user@ip-172-31-46-238.ec2.internal>
2020-01-05 17:04:05 +0900

    0105

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,12 +5,21 @@ module UsersHelper
    session[:user_id] = user.id
   end
     
-# 現在サインイン中のユーザー情報を返す
+  # 現在サインイン中のユーザー情報を返す
   def current_user
     if @current_user.nil?
       @current_user = User.find_by(id: session[:user_id])
     else
       @current_user
+    end
+  end
+  
+  # プロフィール画像がなかったらダミー画像を指定する
+  def image_url(user)
+    if user.image.blank?
+      "https://dummyimage.com/200x200/000/fff"
+    else
+      "/users/#{user.image}"
     end
   end
   
